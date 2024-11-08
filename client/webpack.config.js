de6148155268823a27051ad4e6caeeaafaee23e0
S-via <168160925+S-via@users.noreply.github.com>
2024-11-08 13:14:28 -0800

    added concurrently for npm starts and added into webpack logic for webpackmanifest, injectmaifest, html webpackplugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //added html
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Nineteen'
+      }),
+
+      // added inject manifest
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // added wepackmanifest // reference to mini proj
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'Nineteen',
+        description: 'Offline Text Editor',
+        backround_color: '#bfa0da',
+        theme_color: '#bfa0da',
+        start_url: './',
+        publicPath: './',
+
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            size: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+
+          },
+
+        ],
+      }),
+
     ],
 
     module: {
       rules: [
-        
+        // added from mini project 19 
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_module/,
+
+          //  added babel-loader
+          use: {
+            loader: 'babel-loader',
+            option: {
+              presets: ['@babel/preset-env'],
+
+            }
+          }
+        }
+
+
       ],
     },
   };
